a850e0b82ad3823649947578e28887b6fff888b9
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-17 18:12:45 +0300

    Add data struct

--- apptypes/apptypes.go
+++ apptypes/apptypes.go
@@ -1,1 +1,12 @@
 package apptypes
+
+import "time"
+
+type Data struct {
+	TrainId            int
+	DepartureStationId int
+	ArrivalStationId   int
+	Cost               float64
+	DepartureTime      time.Time
+	ArrivalTime        time.Time
+}
