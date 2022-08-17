a09d08bfe56d50079846d657dfad1711360aba42
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-17 18:13:04 +0300

    Write csv parser

---
+++ utils/utils.go
@@ -0,0 +1,73 @@
+package utils
+
+import (
+	"DistributedLab_Trains/apptypes"
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func parseCsvToDataStruct(path string) ([]apptypes.Data, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Panicln("Open file error:", err)
+		return nil, err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Panicln("Close file error:", err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	var result []apptypes.Data
+
+	for scanner.Scan() {
+		var dataTmp apptypes.Data
+		seperatedValues := strings.Split(strings.ReplaceAll(scanner.Text(), " ", ""), ";")
+		err := parseCsvLine(&dataTmp, seperatedValues)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, dataTmp)
+	}
+	return result, nil
+}
+
+func parseCsvLine(data *apptypes.Data, lines []string) error {
+	conv, err := strconv.Atoi(lines[0])
+	if err != nil {
+		return err
+	}
+	data.TrainId = conv
+	conv, err = strconv.Atoi(lines[1])
+	if err != nil {
+		return err
+	}
+	data.DepartureStationId = conv
+	conv, err = strconv.Atoi(lines[2])
+	if err != nil {
+		return err
+	}
+	data.ArrivalStationId = conv
+	convPrice, err := strconv.ParseFloat(lines[3], 64)
+	if err != nil {
+		return err
+	}
+	data.Cost = convPrice
+	t, err := time.Parse("15:04:05", lines[4])
+	if err != nil {
+		return err
+	}
+	data.DepartureTime = t
+	t, err = time.Parse("15:04:05", lines[5])
+	if err != nil {
+		return err
+	}
+	data.ArrivalTime = t
+	return nil
+}
