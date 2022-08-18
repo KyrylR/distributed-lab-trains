5a89d1d2fe4a19c3eb057fdf61435e4b54be9e96
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-18 16:35:56 +0300

    Add PathTree, PossibleWay, Path structs (start work session)

--- apptypes/apptypes.go
+++ apptypes/apptypes.go
@@ -2,11 +2,31 @@ package apptypes
 
 import "time"
 
-type Data struct {
+type Train struct {
 	TrainId            int
 	DepartureStationId int
 	ArrivalStationId   int
 	Cost               float64
 	DepartureTime      time.Time
 	ArrivalTime        time.Time
+}
+
+type PathTree struct {
+	Id              int
+	DepartureId     int
+	ArrivalStations map[int]bool
+	Routes          []Train
+	Next            *[]*PathTree
+}
+
+type PossibleWay struct {
+	LastArrival int
+	Way         []int
+	TrainMap    map[int][]Train
+}
+
+type Path struct {
+	Trains        []Train
+	TotalCost     int
+	TotalDuration time.Time
 }
