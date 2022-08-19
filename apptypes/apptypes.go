9cdd491c30270e19c6261cf7060111ab30321d77
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-19 10:43:31 +0300

    Refactor apptypes, delete unused fields

--- apptypes/apptypes.go
+++ apptypes/apptypes.go
@@ -2,6 +2,7 @@ package apptypes
 
 import "time"
 
+// Train structure represent CSV from data file
 type Train struct {
 	TrainId            int
 	DepartureStationId int
@@ -11,22 +12,7 @@ type Train struct {
 	ArrivalTime        time.Time
 }
 
-type PathTree struct {
-	Id              int
-	DepartureId     int
-	ArrivalStations map[int]bool
-	Routes          []Train
-	Next            *[]*PathTree
-}
-
 type PossibleWay struct {
-	LastArrival int
-	Way         []int
-	TrainMap    map[int][]Train
-}
-
-type Path struct {
-	Trains        []Train
-	TotalCost     int
-	TotalDuration time.Time
+	Way      []int
+	TrainMap map[int][]Train
 }
