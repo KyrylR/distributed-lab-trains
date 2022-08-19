676fec649c60b17309ed843f87d88bba9bb816a9
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-19 15:50:34 +0300

    Finish brute force method

--- algoritms/bruteForce.go
+++ algoritms/bruteForce.go
@@ -1,21 +1,47 @@
 package algoritms
 
 import (
-	"DistributedLab_Trains/apptypes"
 	"fmt"
 )
 
-func BuildPaths(data *[]apptypes.Train) {
+func BuildPaths(data *[]Train) {
 	uniqueStations := GetUniqueStations(data, false)
 	mappedData := BuildMappedData(data)
-	pathTree := PathTree{DepartureId: 1}
-	pathTree.buildPathTree(uniqueStations, mappedData)
-	fmt.Println(pathTree)
-	//for _, station := range uniqueStations {
-	//	result := buildRoutesFromDeparture(data, station)
-	//	for _, posWay := range result {
-	//		fmt.Println(posWay.Way)
-	//	}
-	//}
-	//buildRoutesFromDeparture(data, 3)
+	for _, station := range uniqueStations {
+		pathTree := PathTree{DepartureId: station}
+		treeMap := pathTree.buildPathTree(uniqueStations, mappedData)
+		pathTrees := getAllPathTrees(&pathTree, &treeMap)
+		ways := buildNewWaysFromPathTree(pathTrees, len(uniqueStations))
+		for _, way := range ways.Ways {
+			fmt.Println(way.Way)
+		}
+	}
+}
+
+func buildNewWaysFromPathTree(pathTrees []*PathTree, maxStations int) Ways {
+	ways := Ways{}
+
+	for i := 0; i < maxStations+1; i++ {
+		for _, offer := range pathTrees {
+			ways.proposeWay(offer, maxStations)
+		}
+	}
+
+	ways.clear(maxStations)
+	return ways
+}
+
+func getAllPathTrees(tree *PathTree, treeMap *map[int]*PathTree) []*PathTree {
+	start := tree.DepartureId
+
+	result := []*PathTree{tree}
+	i := 1
+	for k, v := range *treeMap {
+		if k != start {
+			result = append(result, v)
+			i++
+		}
+	}
+
+	return result
 }
