0fbdc71d8fd4b8a65af294442b3b306f9aae2e4a
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-19 15:50:02 +0300

    Fix bug with Copy

--- algoritms/ways.go
+++ algoritms/ways.go
@@ -1,6 +1,8 @@
 package algoritms
 
-import "reflect"
+import (
+	"reflect"
+)
 
 type PossibleWay struct {
 	Way      []int
@@ -8,10 +10,37 @@ type PossibleWay struct {
 }
 
 type Ways struct {
-	Ways []PossibleWay
+	Ways []*PossibleWay
 }
 
-func (ways *Ways) isWayNeeded(newWay []int) bool {
+func (ways *Ways) proposeWay(pathNode *PathTree, maxStations int) bool {
+	addedWay, justAdded := true, false
+	if len(ways.Ways) == 0 {
+		for addedWay {
+			addedWay = ways.addWay(PossibleWay{}, pathNode, maxStations)
+		}
+		return true
+	}
+	for _, way := range ways.Ways {
+		tempWey := *way
+		addedWay = way.proposeNewWay(pathNode, ways, maxStations)
+		for addedWay {
+			justAdded = true
+			addedWay = ways.addWay(tempWey, pathNode, maxStations)
+		}
+	}
+	return justAdded
+}
+
+func (ways *Ways) addWay(newWay PossibleWay, pathNode *PathTree, maxStations int) bool {
+	if newWay.proposeNewWay(pathNode, ways, maxStations) {
+		ways.Ways = append(ways.Ways, &newWay)
+		return true
+	}
+	return false
+}
+
+func (ways Ways) isWayNeeded(newWay []int) bool {
 	for _, way := range ways.Ways {
 		if way.isEqual(newWay) {
 			return false
@@ -20,24 +49,33 @@ func (ways *Ways) isWayNeeded(newWay []int) bool {
 	return true
 }
 
-func (w *PossibleWay) proposeNewWay(pathNode *PathTree, ways Ways, maxStations int) bool {
-	if maxStations >= len(w.Way) || maxStations <= 0 {
+func (ways *Ways) clear(maxStations int) {
+	for i := 0; i < len(ways.Ways); i++ {
+		if len(ways.Ways[i].Way) != maxStations {
+			ways.Ways = append(ways.Ways[:i], ways.Ways[i+1:]...)
+			i-- // Since we just deleted a[i], we must redo that index
+		}
+	}
+}
+
+func (w *PossibleWay) proposeNewWay(pathNode *PathTree, ways *Ways, maxStations int) bool {
+	if maxStations <= len(w.Way) || maxStations <= 0 {
 		return false
 	}
 	if len(w.Way) == 0 {
 		w.Way = append(w.Way, pathNode.DepartureId)
-		return w.addWay(pathNode, ways)
+		return w.addNewWay(pathNode, ways)
 	}
 	if w.getLastStation() == pathNode.DepartureId {
-		return w.addWay(pathNode, ways)
+		return w.addNewWay(pathNode, ways)
 	}
 	return false
 }
 
-func (w *PossibleWay) addWay(pathNode *PathTree, ways Ways) bool {
+func (w *PossibleWay) addNewWay(pathNode *PathTree, ways *Ways) bool {
 	for _, arrv := range pathNode.Next {
 		if !IsStationAlreadyPassed(w.Way, arrv.DepartureId) {
-			newWay := append(w.Way, pathNode.DepartureId)
+			newWay := append(w.copy(), arrv.DepartureId)
 			if ways.isWayNeeded(newWay) {
 				w.Way = newWay
 				w.addRoutes(pathNode, arrv.DepartureId)
@@ -48,18 +86,29 @@ func (w *PossibleWay) addWay(pathNode *PathTree, ways Ways) bool {
 	return false
 }
 
+func (w PossibleWay) copy() []int {
+	copiedWay := make([]int, len(w.Way))
+	for idx, item := range w.Way {
+		copiedWay[idx] = item
+	}
+	return copiedWay
+}
+
 func (w *PossibleWay) addRoutes(pathNode *PathTree, toStation int) {
 	for _, route := range pathNode.Routes {
-		if w.getLastStation() == toStation {
+		if route.ArrivalStationId == toStation {
+			if w.TrainMap == nil {
+				w.TrainMap = make(map[int][]Train)
+			}
 			w.TrainMap[route.DepartureStationId] = append(w.TrainMap[route.DepartureStationId], route)
 		}
 	}
 }
 
-func (w *PossibleWay) getLastStation() int {
+func (w PossibleWay) getLastStation() int {
 	return w.Way[len(w.Way)-1]
 }
 
-func (w *PossibleWay) isEqual(other []int) bool {
+func (w PossibleWay) isEqual(other []int) bool {
 	return reflect.DeepEqual(w.Way, other)
 }
