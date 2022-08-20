484ae4c68aee6119b2681ff943f54b41a1e7c84c
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-20 18:02:59 +0300

    Add test for myApproach

---
+++ tests/algoritm_test.go
@@ -0,0 +1,24 @@
+package tests
+
+import (
+	"testing"
+
+	"DistributedLab_Trains/algoritms"
+	"DistributedLab_Trains/utils"
+)
+
+func TestMyApproach(t *testing.T) {
+	allTrains, err := utils.ParseCsvToTrainStruct("data_for_tests.csv")
+	if err != nil {
+		t.Errorf("error ocured when parsing csv file")
+		return
+	}
+	paths := algoritms.BuildPaths(&allTrains)
+	sum := 0
+	for _, path := range paths {
+		sum += len(path.Ways)
+	}
+	if sum != 10 {
+		t.Errorf("incorect number of founded ways, given: %v, should be: %v", sum, 10)
+	}
+}
