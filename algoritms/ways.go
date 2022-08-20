f0f71378e590704ffc2258acf428029155d0fc14
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-20 16:22:46 +0300

    Added documentation for PossibleWay and Ways structures and related functions.

--- algoritms/ways.go
+++ algoritms/ways.go
@@ -4,15 +4,21 @@ import (
 	"reflect"
 )
 
+// PossibleWay represents the path from station to station and the associated
+// trains that can be used to get to the desired stations.
 type PossibleWay struct {
 	Way      []int
-	TrainMap map[int][]Train
+	TrainMap map[int][]Train // key - station identifier, value - array of similar trains.
 }
 
+// Ways represents an array of PossibleWay references.
 type Ways struct {
 	Ways []*PossibleWay
 }
 
+// proposeWay as function is called, it `proposes` a given PathTree object to every PossibleWay in the Ways structure.
+// Returns FALSE if there are no possible paths in which the ID of the last station matches the DepartureId field of
+// the PathTree object, otherwise it tries to add a new path the required number of times and returns TRUE at the end.
 func (ways *Ways) proposeWay(pathNode *PathTree, maxStations int) bool {
 	addedWay, justAdded := true, false
 	if len(ways.Ways) == 0 {
@@ -32,6 +38,9 @@ func (ways *Ways) proposeWay(pathNode *PathTree, maxStations int) bool {
 	return justAdded
 }
 
+// addWay takes an instance of PossibleWay, a reference to pathTree and the number of unique stations in the data.
+// Calls proposeNewWay function, and if it returns true, we add new path to the array and return true,
+// otherwise we return false.
 func (ways *Ways) addWay(newWay PossibleWay, pathNode *PathTree, maxStations int) bool {
 	if newWay.proposeNewWay(pathNode, ways, maxStations) {
 		ways.Ways = append(ways.Ways, &newWay)
@@ -40,15 +49,7 @@ func (ways *Ways) addWay(newWay PossibleWay, pathNode *PathTree, maxStations int
 	return false
 }
 
-func (ways *Ways) isWayNeeded(newWay []int) bool {
-	for _, way := range ways.Ways {
-		if way.isEqual(newWay) {
-			return false
-		}
-	}
-	return true
-}
-
+// clear - removes any way that do not contain all stations
 func (ways *Ways) clear(maxStations int) {
 	for i := 0; i < len(ways.Ways); i++ {
 		if len(ways.Ways[i].Way) != maxStations {
@@ -58,6 +59,18 @@ func (ways *Ways) clear(maxStations int) {
 	}
 }
 
+// isWayNeeded returns true if the provided array of station IDs is unique in the current PossibleWay array
+// of the Ways structure, otherwise it returns false.
+func (ways *Ways) isWayNeeded(newWay []int) bool {
+	for _, way := range ways.Ways {
+		if way.isEqual(newWay) {
+			return false
+		}
+	}
+	return true
+}
+
+// proposeNewWay checks for path constraints, if all is OK, calls addNewWay function, otherwise returns false.
 func (w *PossibleWay) proposeNewWay(pathNode *PathTree, ways *Ways, maxStations int) bool {
 	if maxStations <= len(w.Way) || maxStations <= 0 {
 		return false
@@ -72,13 +85,18 @@ func (w *PossibleWay) proposeNewWay(pathNode *PathTree, ways *Ways, maxStations 
 	return false
 }
 
+// addNewWay takes reference to PathTree, where field DepartureId corresponds to the last station in the path;
+// and a reference to the actual Ways. It checks if the station has already been passed
+// and if its path in Ways is unique. If the new path is unique in the current array of stations
+// and unique in the PossibleWay array in the Ways structure, it is added to the current array and returns true,
+// otherwise false is returned.
 func (w *PossibleWay) addNewWay(pathNode *PathTree, ways *Ways) bool {
-	for _, arrv := range pathNode.Next {
-		if !IsStationAlreadyPassed(w.Way, arrv.DepartureId) {
-			newWay := append(w.copy(), arrv.DepartureId)
+	for _, arrival := range pathNode.Next {
+		if !IsStationAlreadyPassed(w.Way, arrival.DepartureId) {
+			newWay := append(w.copy(), arrival.DepartureId)
 			if ways.isWayNeeded(newWay) {
 				w.Way = newWay
-				w.addRoutes(pathNode, arrv.DepartureId)
+				w.addRoutes(pathNode, arrival.DepartureId)
 				return true
 			}
 		}
@@ -86,14 +104,8 @@ func (w *PossibleWay) addNewWay(pathNode *PathTree, ways *Ways) bool {
 	return false
 }
 
-func (w *PossibleWay) copy() []int {
-	copiedWay := make([]int, len(w.Way))
-	for idx, item := range w.Way {
-		copiedWay[idx] = item
-	}
-	return copiedWay
-}
-
+// addRoutes searches all trains that are in the Routes' field of the PathTree structure;
+// fills TrainMap field in PossibleWay with those trains that have the appropriate arrival station ID (toStation).
 func (w *PossibleWay) addRoutes(pathNode *PathTree, toStation int) {
 	for _, route := range pathNode.Routes {
 		if route.ArrivalStationId == toStation {
@@ -105,10 +117,21 @@ func (w *PossibleWay) addRoutes(pathNode *PathTree, toStation int) {
 	}
 }
 
-func (w *PossibleWay) getLastStation() int {
-	return w.Way[len(w.Way)-1]
+// copy make a copy of Way
+func (w *PossibleWay) copy() []int {
+	copiedWay := make([]int, len(w.Way))
+	for idx, item := range w.Way {
+		copiedWay[idx] = item
+	}
+	return copiedWay
 }
 
+// isEqual returns true if the station path is the same, otherwise returns false
 func (w *PossibleWay) isEqual(other []int) bool {
 	return reflect.DeepEqual(w.Way, other)
+}
+
+// getLastStation returns the ID of the last station in the Way
+func (w *PossibleWay) getLastStation() int {
+	return w.Way[len(w.Way)-1]
 }
