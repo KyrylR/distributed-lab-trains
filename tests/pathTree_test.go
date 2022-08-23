84fa831d62375a326a70aba1fac150be1eda50b0
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-23 10:41:32 +0300

    Additional data checks added

--- tests/pathTree_test.go
+++ tests/pathTree_test.go
@@ -14,12 +14,26 @@ func TestPathTree(t *testing.T) {
 		return
 	}
 	uniqueStations := findPath.GetUniqueStations(&allTrains, false)
+	if len(uniqueStations) != 4 {
+		t.Errorf("incorect number of unique station found, given: %v, should be: %v",
+			len(uniqueStations), 4)
+	}
+	NumberOfTrainsThatDepFromStation := []int{2, 3, 3, 1}
 	mappedData := findPath.BuildMappedData(&allTrains)
+	i := 0
+	for _, value := range mappedData {
+		if len(value) != NumberOfTrainsThatDepFromStation[i] {
+			t.Errorf("incorect number of trains fro station calculated, given: %v, should be: %v",
+				len(value), NumberOfTrainsThatDepFromStation[i])
+		}
+		i += 1
+	}
 	pathTree := findPath.PathTree{DepartureId: 1}
 	treeMap := pathTree.BuildPathTree(uniqueStations, mappedData)
 	pathTrees := findPath.GetAllPathTrees(&pathTree, &treeMap)
 	if len(pathTrees) != 4 {
-		t.Errorf("incorectly determinid home many unique sities in the data, given: %v, should be: %v", len(pathTrees), 4)
+		t.Errorf("incorectly determinid home many unique stations in the data, given: %v, should be: %v",
+			len(pathTrees), 4)
 	}
 
 	for _, tree := range pathTrees {
